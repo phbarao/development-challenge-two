2fe58f922b726f3e47e86ee0f9f09d335e133ec1
Pedro Barão <phbarao@gmail.com>
2022-01-05 18:14:06 -0300

    style: create global style

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import GlobalStyles from './styles/global';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
+
+    <GlobalStyles />
   </React.StrictMode>,
   document.getElementById('root')
 );
