1445ad16f06af269a52fbf3d3a87e4068eb61bd9
Pedro Barão <phbarao@gmail.com>
2022-01-05 20:07:25 -0300

    feat: list patient names on dashboard

--- front-end/src/pages/Dashboard/index.js
+++ front-end/src/pages/Dashboard/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
 
 function Dashboard() {
+  const [patients, setPatients] = useState([]);
+
+  useEffect(() => {
+    async function loadPatients() {
+      const items = await api.get('/patients');
+
+      setPatients(items.data.Items);
+    }
+
+    loadPatients();
+  }, []);
+
   return (
     <>
       <h1>Dashboard</h1>
-      <Link to="/create-patient">Cadastrar pacient</Link>
+      <ul>
+        {patients.map((patient) => (
+          <li key={patient.id}>{patient.name}</li>
+        ))}
+      </ul>
     </>
   );
 }
