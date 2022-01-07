94e5a5a9f306a00793fa9ead0be80af363d5e92f
Pedro Barão <phbarao@gmail.com>
2022-01-06 22:49:47 -0300

    feat: updtate routes

--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Dashboard from '../pages/Dashboard';
+import Landing from '../pages/Landing';
+import ListPatients from '../pages/ListPatients';
 import CreatePatient from '../pages/CreatePatient';
 
 function Routes() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/" component={Landing} />
+        <Route path="/list-patients" component={ListPatients} />
         <Route path="/create-patient" component={CreatePatient} />
       </Switch>
     </Router>
